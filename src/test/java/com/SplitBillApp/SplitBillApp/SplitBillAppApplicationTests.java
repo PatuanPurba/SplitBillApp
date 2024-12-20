695b7529b93683b547c1b02a4bcef90016e951eb
Patuan Purba <58598879+PatuanPurba@users.noreply.github.com>
2024-12-20 10:39:55 -0500

    Spring Boot Initializer 2
    Second Initializer

---
+++ src/test/java/com/SplitBillApp/SplitBillApp/SplitBillAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SplitBillApp.SplitBillApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SplitBillAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
